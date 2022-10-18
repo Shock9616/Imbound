6a74d9c60baa60f7be2f6e701076ecc277a60f48
Kaleb Rosborough <kalebrosborough@Kalebs-MBP.ad.ufv.ca>
2022-10-18 11:58:18 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+/*
+index.js
+Imbound
+
+Created by Kaleb Rosborough on 10 / 17 / 2022
+Part of a group project by Max F, Max Z, Kaleb R, Norman A, Dylan A
+*/
+
+document.addEventListener("DOMContentLoaded", () => {});
