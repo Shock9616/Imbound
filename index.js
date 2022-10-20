6c6b0bdb0283fdddd73816407e97008f46da2247
Shock9616 <Shock9616@users.noreply.github.com>
2022-10-20 10:28:06 -0700

    Added new pages and implemented basic form validation

--- index.js
+++
@@ -1,9 +0,0 @@
-/*
-index.js
-Imbound
-
-Created by Kaleb Rosborough on 10 / 17 / 2022
-Part of a group project by Max F, Max Z, Kaleb R, Norman A, Dylan A
-*/
-
-document.addEventListener("DOMContentLoaded", () => {});
