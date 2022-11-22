f4bf9f8db566d3657af806121b25f9962ea8b8ec
CautionWetPaint <116135288+CautionWetPaint@users.noreply.github.com>
2022-11-22 10:57:20 -0800

    Add files via upload

---
+++ profile.js
@@ -0,0 +1,12 @@
+/* Created by Max F. */
+$(document).ready( ()=> 
+    $("#slider").bxSlider({
+    auto:true,
+    pause: 5000,
+    autoStart:true,
+    autoDirection:"next",
+    autoHover:true,
+})
+);
+
+
