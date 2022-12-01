e8f79cac5e11deb6f61a8ca5abb2c4d7db9fa6da
Shock9616 <Shock9616@users.noreply.github.com>
2022-12-01 10:05:33 -0800

    Styled menu bar on the about page

--- scripts/homepage.js
+++ scripts/homepage.js
@@ -9,14 +9,6 @@ Part of a group project by Max F, Max Z, Kaleb R, Norman A, Dylan A
 "use strict";
 
 $(document).ready(() => {
-    // Arrange the images in a gallery that fills
-    // empty space between differently sized images
-    $('#img_gallery').masonry({
-        itemSelector: ".grid_item",
-        columnWidth: 200,
-        fitWidth: true
-    });
-
     // When an image is clicked, remember that
     // image and redirect the user to detail.html
     $(".grid_item").click(function () {
@@ -24,3 +16,11 @@ $(document).ready(() => {
         window.location.href = "detail.html";
     });
 });
+
+// Arrange the images in a gallery that fills
+    // empty space between differently sized images
+$('#img_gallery').masonry({
+        itemSelector: ".grid_item",
+        columnWidth: 200,
+        fitWidth: true
+    });
